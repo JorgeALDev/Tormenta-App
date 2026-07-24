01c80f693f68726d778f676a86f57ebaefad8efd
JorgeALDev <jorgearaujo1854@gmail.com>
2026-07-24 11:44:33 -0300

    Livros: Dois novos livros adicionados, Deuses de Arton e Guia de Deuses menores, um livro atualizado para a versão mais recente, Atlas de Arton

--- app/src/main/java/com/T20/tormentaapp/ui/livros/LivrosData.kt
+++ app/src/main/java/com/T20/tormentaapp/ui/livros/LivrosData.kt
@@ -9,25 +9,29 @@ object LivrosData {
             capa = R.drawable.capalivrobase,
             url = "https://online.anyflip.com/tawfh/fsde/mobile/index.html"
         ),Livro(
-            titulo = "Heróis de Arton",
-            capa = R.drawable.capaheroisarton,
-            url = "https://online.anyflip.com/gjwhv/zugl/mobile/index.html"
-        ),Livro(
-            titulo = "Atlas de Arton",
-            capa = R.drawable.capaatlasarton,
-            url = "https://online.anyflip.com/dyxol/zvig/mobile/index.html"
-        ),Livro(
             titulo = "Ameaças de Arton",
             capa = R.drawable.capaameacasarton,
             url = "https://online.anyflip.com/arbyd/nccd/mobile/index.html"
         ),Livro(
-            titulo = "A Voz das Marés - Uma Aventura Introdutória (Para personagens de 1° Nível)",
+            titulo = "Atlas de Arton",
+            capa = R.drawable.capaatlasarton,
+            url = "https://online.anyflip.com/ntfeh/tpky/mobile/index.html"
+        ),Livro(
+            titulo = "Deuses de Arton",
+            capa = R.drawable.capadeusesarton,
+            url = "https://online.anyflip.com/gjwhv/cfnx/mobile/index.html"
+        ),Livro(
+            titulo = "Heróis de Arton",
+            capa = R.drawable.capaheroisarton,
+            url = "https://online.anyflip.com/gjwhv/zugl/mobile/index.html"
+        ),Livro(
+            titulo = "Guia de Deuses Menores",
+            capa = R.drawable.capaguiadeusesmenores,
+            url = "https://online.anyflip.com/gjwhv/lxau/mobile/index.html"
+        ),Livro(
+            titulo = "A Voz das Marés - Uma Aventura Introdutória",
             capa = R.drawable.capavozdasmares,
             url = "https://online.anyflip.com/ltqmw/soyl/mobile/index.html"
-        ),Livro(
-            titulo = "Bestiário de Arton",
-            capa = R.drawable.capabestiarioarton,
-            url = "https://online.anyflip.com/yfsba/wbdp/mobile/index.html"
-        ),
+        )
     )
 }
